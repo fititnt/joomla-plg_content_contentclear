b025b126c8ad5a4f7738083302acfbf49a6b86f3
Emerson Rocha Luiz <emerson@alligo.com.br>
2018-04-01 19:47:45 -0300

    Fixed plugin not being called;

--- contentclear.php
+++ contentclear.php
@@ -13,8 +13,31 @@ defined('_JEXEC') or die;
  *
  * @since  1.6
  */
-class PlgContentclear extends JPlugin
+class PlgContentContentclear extends JPlugin
 {
+
+	/**
+	 * see https://www.sitepoint.com/community/t/remove-inline-style-with-preg-replace/21743/3
+	 * see https://stackoverflow.com/questions/5517255/remove-style-attribute-from-html-tags
+	 * see https://stackoverflow.com/questions/7740249/how-to-strip-style-attributes-from-html-tags
+	 **/
+	private function _clean($htmlstring) {
+		$domd = new DOMDocument();
+		libxml_use_internal_errors(true);
+		$domd->loadHTML($html);
+		libxml_use_internal_errors(false);
+
+		$domx = new DOMXPath($domd);
+		$items = $domx->query("//span[@style]");
+		
+		foreach($items as $item) {
+		  $item->removeAttribute("style");
+		}
+		
+		return $domd->saveHTML();
+	}
+
+
 	/**
 	 * This is an event that is called right before the content is saved into 
 	 * the database.
@@ -30,15 +53,26 @@ class PlgContentclear extends JPlugin
 	 */
 	public function onContentBeforeSave($context, $article, $isNew)
 	{
+
+		//die();
 		// Check we are handling the frontend edit form.
-		if ($context !== 'com_content.form')
+		if ($context !== 'com_content.article')
 		{
 			return true;
 		}
 
+
+		// print_r($article->introtext. 'TEEESTE 1111');
+		// print_r($this->_clean($article->introtext) . 'TEEESTE 2222');
+		// die ();
+		$article->introtext = 'oioioi' . $article->introtext . $this->_clean($article->introtext);
+		echo $article->introtext ;
+		die;
+		// $article->introtext = $this->_clean($article->introtext);
+
 		//...,
 
-		return $result;
+		return true;
 	}
 
 }
